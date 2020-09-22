0eb87064d039f0f6e246ec7e1137aaff4f8232b3
Ralph F <RaftsDev@gmail.com>
2020-09-22 12:21:29 -0400

    Feat: Check Math class to create subclasses and instances.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // Create instance of Math - Error private acces to constructor.
+        //Math m = new Math();
+
+        //Create subclasses of Math - Error Math class is Final
+        /*public class m extends Math {
+
+        }*/
+
+        System.out.println("PI="+Math.PI);// PI - static final constant
+
     }
 }
